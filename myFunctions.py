f044fdb41379dd70caa7211e81e6170ee456bbe1
TensorMousie <heleman44@protonmail.com>
2023-07-11 21:06:13 +0200

    i am mousiesie

--- myFunctions.py
+++ myFunctions.py
@@ -3,6 +3,6 @@ import os
 def read_file(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
-        print('hello bob')
+        print('hello bob ope')
     return content
 
